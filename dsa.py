f91b83115cf10e32d27eac268cf8a880efb140ea
idrice junior <dongmojunior18@gmail.com>
2026-06-29 12:27:02 -0500

    gitdsyt

--- dsa.py
+++ dsa.py
@@ -1,17 +1,17 @@
-first = [4,5,7,1,23]
+first = [1,2,3,4,5,6]
 second = []
 n = len(first)
-num = 1
+num = 3
 middle = (n)//2
-print(first[middle])
+#print(first[middle])
 for i in range(n):
-    if first[middle]< num:
-        second.append[middle+1]
+    if first[middle]<= num:
+        second.append(middle+1)
         middle = (n)//2
-    elif first[middle]> num:
-        second.append[middle-1]
+    elif first[middle]>= num:
+        second.append(middle-1)
         middle = (n)//2
 
     else:
-        print(second[middle])
+        print(middle)
 
